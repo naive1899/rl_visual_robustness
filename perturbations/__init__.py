2eb25de593593611bf5216a0705e12f5ee6e07fc
naive1899 <naive1899@gmail.com>
2026-04-29 18:42:30 +0300

    Initialize __init__.py with visual perturbations

---
+++ perturbations/__init__.py
@@ -0,0 +1,21 @@
+from .visual_perturbations import (
+    VisualPerturbation,
+    ColorJitter,
+    RandomDropout,
+    GaussianBlur,
+    GaussianNoise,
+    SaltPepperNoise,
+    Pixelate,
+    PerturbationManager
+)
+
+__all__ = [
+    'VisualPerturbation',
+    'ColorJitter', 
+    'RandomDropout',
+    'GaussianBlur',
+    'GaussianNoise',
+    'SaltPepperNoise',
+    'Pixelate',
+    'PerturbationManager'
+]
